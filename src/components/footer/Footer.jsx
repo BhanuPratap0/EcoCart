876ac951b606f3c14892869d1d7d17ea91e831dd
Bhanu Pratap Singh <119663548+BhanuPratap0@users.noreply.github.com>
2024-02-23 11:47:34 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <>
             <section id="copy-right">
                 <div class="copy-right-sec"><i class="fa-solid fa-copyright"></i>
-                    lorem ispum lorem ispum 2022 Powerd By <a href="#">lorem ispum</a>
+                    Made with ❤️ by<a href="#">Bhanu</a>
                 </div>
             </section>
         </>
